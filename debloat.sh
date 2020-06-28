6fc96b56fd26ef0a9a897da256983a9551329b8a
struct_dhancha <jash.gopani@somaiya.edu>
2020-06-28 20:59:29 +0530

    Updated Script

--- debloat.sh
+++ debloat.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-printf "Initiating debloat\n"
-
+clear
 ## put all the complete app names in an array
 declare -a arr=("com.mipay.wallet.in"
 				"com.xiaomi.payment"
@@ -24,12 +23,43 @@ declare -a arr=("com.mipay.wallet.in"
 				"com.miui.cloudbackup"
 				)
 
+##Do not edit code beyond this point if you don't understand
+
+printf "********************************************************\n"
+printf "                 Welcome to Debloat\n"
+printf "********************************************************\n\n"
+
+read -s -p "Press any key to continue..."
+
+clear
+printf "********************************************************\n"
+printf "                 Welcome to Debloat\n"
+printf "********************************************************\n\n"
+
+##show the users the list to be uninstalled
+printf "\n\nThe following apps will be uninstalled from your device\n\n"
+for i in "${arr[@]}"
+do
+   echo "$i"
+   sleep 0.1
+done
+
+printf "\n\nNote: You can edit this List\n\n"
+read -p "Are you sure you want to Uninstall all of them (Y or N)? " yn
+    case $yn in
+        [Yy]* ) printf "Initiating Debloat...\n\n";continue; break;;
+        * ) printf "Terminating Application...\n\n";exit 0;break;;
+    esac
+clear
+printf "********************************************************\n"
+printf "                 Debloating Your device\n"
+printf "********************************************************\n\n"
 ## loop and uninstall each app
 for i in "${arr[@]}"
 do
    echo "Uninstalling $i"
-   echo ""
    adb shell pm uninstall -k --user 0 "$i"
+   sleep 0.3
    printf "\n"
 done
 
@@ -43,4 +73,7 @@ read -p "Do you want to reboot your device ? " yn
         * ) echo "";;
     esac
 
-echo "Debloat complete !"
+clear
+printf "********************************************************\n"
+printf "                 Debloat Complete\n"
+printf "********************************************************\n\n"
