9957fc5548d4d546822bc91734eb302d2202465d
struct_dhancha <jash.gopani@somaiya.edu>
2020-06-28 21:08:15 +0530

    minor changes

--- debloat.sh
+++ debloat.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+clear
 ## put all the complete app names in an array
 declare -a arr=("com.mipay.wallet.in"
 				"com.xiaomi.payment"
@@ -30,30 +30,35 @@ printf "                 Welcome to Debloat\n"
 printf "********************************************************\n\n"
 
 read -s -p "Press any key to continue..."
-printf "\n"
 
 clear
+printf "********************************************************\n"
+printf "                 Welcome to Debloat\n"
+printf "********************************************************\n\n"
 
 ##show the users the list to be uninstalled
 printf "\n\nThe following apps will be uninstalled from your device\n\n"
 for i in "${arr[@]}"
 do
    echo "$i"
-   sleep 0.31
+   sleep 0.1
 done
 
 printf "\n\nNote: You can edit this List\n\n"
 read -p "Are you sure you want to Uninstall all of them (Y or N)? " yn
     case $yn in
         [Yy]* ) printf "Initiating Debloat...\n\n";continue; break;;
-        * ) echo "Terminating Application...\n\n";exit 0;break;;
+        * ) printf "Terminating Application...\n\n";exit 0;break;;
     esac
-
-
+clear
+printf "********************************************************\n"
+printf "                 Debloating Your device\n"
+printf "********************************************************\n\n"
 ## loop and uninstall each app
 for i in "${arr[@]}"
 do
    echo "Uninstalling $i"
+#    adb shell pm uninstall -k --user 0 "$i"
    sleep 0.3
    echo "Success"
    printf "\n"
@@ -69,4 +74,7 @@ read -p "Do you want to reboot your device ? " yn
         * ) echo "";;
     esac
 
-echo "Debloat complete !"
+clear
+printf "********************************************************\n"
+printf "                 Debloat Complete\n"
+printf "********************************************************\n\n"
